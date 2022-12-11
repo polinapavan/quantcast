ef606287144507ae5449ee019a993e8d183740aa
Pavan Kumar <Polina.Kumar@walmart.com>
2022-12-11 22:55:22 +0530

    Adding Negative Scenarios Test Cases

--- src/main/java/org/quantcast/processor/CookiesProcessor.java
+++ src/main/java/org/quantcast/processor/CookiesProcessor.java
@@ -15,6 +15,7 @@ public class CookiesProcessor {
 
     public static final String COMA_DELIMITER = ",";
     public static final String INPUT_SEARCH_DATE_PATTERN = "yyyy-MM-dd";
+    public static final String ERROR_MSG_COOKIE_NOT_FOUND = "No matching cookie found";
 
     public static List<String> getMostActiveCookie(String fileName, String date) {
         List<String> fileLines = writeFileIntoList(fileName);
@@ -24,8 +25,8 @@ public class CookiesProcessor {
 
         int searchIndex = binarySearch(fileLines, searchDate);
         if (searchIndex == -1) {
-            System.out.println("No matching cookie found");
-            throw new RuntimeException("No matching cookie found");
+            System.out.println(ERROR_MSG_COOKIE_NOT_FOUND);
+            throw new RuntimeException(ERROR_MSG_COOKIE_NOT_FOUND);
         }
         Map<String, Integer> cookieCountMap = new HashMap<>();
         processLinesAroundIndex(fileLines, searchDate, cookieCountMap, searchIndex);
@@ -52,7 +53,7 @@ public class CookiesProcessor {
     private static int binarySearch(List<String> fileLines, LocalDate searchDate) {
         int start = 0;
         int end = fileLines.size() - 1;
-        while (end - start >= 0) {
+        while (start <= end) {
             int mid = (end + start) / 2;
             String line = fileLines.get(mid);
             String[] lineSplit = line.split(COMA_DELIMITER);
@@ -63,7 +64,7 @@ public class CookiesProcessor {
                 } else if (cookieDate.isAfter(searchDate)) {
                     start = mid + 1;
                 } else {
-                    end = mid;
+                    end = mid-1;
                 }
             } else {
                 fileLines.remove(mid);
--- src/test/java/org/quantcast/processor/CookiesProcessorTest.java
+++ src/test/java/org/quantcast/processor/CookiesProcessorTest.java
@@ -3,15 +3,18 @@ package org.quantcast.processor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.io.FileNotFoundException;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 
 class CookiesProcessorTest {
 
     @ParameterizedTest
     @CsvFileSource(files = {"src/test/resources/TestParameters/mostActiveCookieTestParams.csv"}, useHeadersInDisplayName = true)
-    void testSuccessGetMostActiveCookie(String fileName, String testDate, String activeCookie) {
-        List<String> derivedActiveCookies = CookiesProcessor.getMostActiveCookie(fileName, testDate);
+    void testSuccessGetMostActiveCookie(String fileName, String searchDate, String activeCookie) {
+        List<String> derivedActiveCookies = CookiesProcessor.getMostActiveCookie(fileName, searchDate);
         String[] expectedActiveCookies = activeCookie.split(";");
         Assertions.assertEquals(expectedActiveCookies.length, derivedActiveCookies.size());
         for (String expectedCookie : expectedActiveCookies) {
@@ -19,4 +22,44 @@ class CookiesProcessorTest {
         }
     }
 
+    @ParameterizedTest
+    @CsvFileSource(files = {"src/test/resources/TestParameters/wrongSearchDateTestParams.csv"}, useHeadersInDisplayName = true)
+    void testSearchDateNotExist(String fileName, String searchDate) {
+        try {
+            CookiesProcessor.getMostActiveCookie(fileName, searchDate);
+        } catch (RuntimeException re) {
+            Assertions.assertEquals(CookiesProcessor.ERROR_MSG_COOKIE_NOT_FOUND, re.getMessage());
+            return;
+        } catch (Exception e) {
+            Assertions.fail("This exception is not expected");
+        }
+        Assertions.fail("Exception is expected here");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"wrongCookieFile.csv", "982374jdlfsjk.csv", "i#(9ad.csv"})
+    void testWrongFileName(String fileName) {
+        try {
+            CookiesProcessor.getMostActiveCookie(fileName, "2018-12-07");
+        } catch (RuntimeException re) {
+            Assertions.assertTrue(re.getCause() instanceof FileNotFoundException);
+            return;
+        } catch (Exception e) {
+            Assertions.fail("This exception is not expected");
+        }
+        Assertions.fail("Exception is expected here");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"2020-98-01", "10-12-2022", "12-23-2022", "12345", "#$@183"})
+    void testWrongSearchDate(String searchDate) {
+        try {
+            CookiesProcessor.getMostActiveCookie("src/test/resources/cookiefiles/cookies.csv", searchDate);
+        } catch (DateTimeParseException re) {
+            return;
+        } catch (Exception e) {
+            Assertions.fail("This exception is not expected");
+        }
+        Assertions.fail("Exception is expected here");
+    }
 }
